1dcbe4df07cdae998f26b8f8816005407986ba87
Gergő <gergo@jedlicska.com>
2020-03-25 10:57:53 +0100

    removed duplicate files

--- structlog_to_seq/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = "0.1.0"
-
-from .celf_processor import CelfProcessor
-
-
-__all__ = ["CelfProcessor"]
--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from structlog_to_seq import CelfProcessor
-
-
-@pytest.fixture(scope="module")
-def celf_processor() -> CelfProcessor:
-    return CelfProcessor()
