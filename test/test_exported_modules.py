5ac9a485f5e5e1aa41907b939ae4e14adcaf6b7d
Gergő <gergo@jedlicska.com>
2020-03-27 09:09:57 +0100

    added version checking in tests with importlib metadata

--- test/test_exported_modules.py
+++ test/test_exported_modules.py
@@ -1,5 +1,15 @@
 from structlog_to_seq import __all__
 
 
-def test_version():
+try:
+    from importlib.metadata import version  # type: ignore
+except ModuleNotFoundError:
+    from importlib_metadata import version  # type: ignore
+
+
+def test_all():
     assert __all__ == ["CelfProcessor", "MessageTemplateRenderer"]
+
+
+def test_version():
+    assert version("structlog_to_seq") == "0.2.0"
