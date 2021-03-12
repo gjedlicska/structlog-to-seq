32a6544d7e49b53acf579a19465c97bdc263d4cc
Gergő <gergo@jedlicska.com>
2021-03-12 12:10:45 +0100

    remove version test

--- test/test_exported_modules.py
+++ test/test_exported_modules.py
@@ -1,15 +1,5 @@
 from structlog_to_seq import __all__
 
 
-try:
-    from importlib.metadata import version  # type: ignore
-except ModuleNotFoundError:
-    from importlib_metadata import version  # type: ignore
-
-
 def test_all():
     assert __all__ == ["CelfProcessor", "MessageTemplateRenderer"]
-
-
-def test_version():
-    assert version("structlog_to_seq") == "0.3.0"
